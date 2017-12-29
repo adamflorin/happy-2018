29221805450baec97acaebf73aa80efe983333c6
Adam Florin <hey@adamflorin.work>
2017-12-28 20:52:41 -0800

    Factor World into class and cut Sequencer

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-import sequencer from './source/Sequencer'
 import {regl} from './source/global'
-import {trigger, tap} from './source/World'
+import physics from './source/Physics'
+import world from './source/World'
 import {playSound} from './source/Audio'
-
-sequencer.setCallback(() => {
-  playSound()
-  trigger()
-})
-
-// sequencer.start()
 
 document.getElementsByTagName('canvas')[0].addEventListener(
   'mousedown',
@@ -16,6 +9,6 @@ document.getElementsByTagName('canvas')[0].addEventListener(
     const x = (event.x / window.innerWidth - 0.5) * 2.0
     const y = (event.y / window.innerHeight - 0.5) * 2.0
     const angle = Math.atan2(-y, x)
-    tap(angle)
+    physics.blow(angle + Math.PI)
   }
 )
--- source/World.js
+++ source/World.js
@@ -7,96 +7,75 @@ import drawMote from './objects/Mote'
 
 const doPostProcess = true
 
-let decay = 0.0
-let decayIntervalId
+class World {
+  constructor() {
+    this._fbo = regl.framebuffer({
+      color: regl.texture({
+        width: 1,
+        height: 1,
+        wrap: 'clamp'
+      }),
+      depth: true
+    })
 
-function tap(angle) {
-  physics.blow(angle + Math.PI)
-}
+    this._captureRaw = regl({
+      cull: {
+        enable: true
+      },
+      framebuffer: this._fbo
+    })
 
-function trigger() {
-  decay = 1.0
-  clearInterval(decayIntervalId)
-  decayIntervalId = setInterval(
-    () => {
-      decay -= 0.05
-      if (decay < 0.0) {
-        decay = 0.0
-        clearInterval(decayIntervalId)
+    this._drawProcessed = regl({
+      vert: require('../shaders/world.vert.glsl'),
+      frag: require('../shaders/world.frag.glsl'),
+      attributes: {
+        position: [ -4, -4, 4, -4, 0, 4 ] // lol render in oversized triangle
+      },
+      uniforms: {
+        tex: () => this._fbo,
+        time: regl.context('time'),
+        width: regl.context('viewportWidth'),
+        height: regl.context('viewportHeight')
+      },
+      depth: {enable: false},
+      count: 3
+    })
+
+    regl.frame(({viewportWidth, viewportHeight}) => {
+      this._fbo.resize(viewportWidth, viewportHeight)
+
+      physics.step()
+
+      if (!doPostProcess) {
+        this._drawScene()
+      } else {
+        this._captureRaw({}, () => this._drawScene())
+        this._drawProcessed()
       }
-    },
-    1000.0 / 60.0
-  )
-}
-
-const fbo = regl.framebuffer({
-  color: regl.texture({
-    width: 1,
-    height: 1,
-    wrap: 'clamp'
-  }),
-  depth: true
-})
-
-const captureRaw = regl({
-  cull: {
-    enable: true
-  },
-  framebuffer: fbo
-})
-
-const drawProcessed = regl({
-  vert: require('../shaders/world.vert.glsl'),
-  frag: require('../shaders/world.frag.glsl'),
-  attributes: {
-    position: [ -4, -4, 4, -4, 0, 4 ] // lol render in oversized triangle
-  },
-  uniforms: {
-    tex: () => fbo,
-    time: regl.context('time'),
-    width: regl.context('viewportWidth'),
-    height: regl.context('viewportHeight')
-  },
-  depth: {enable: false},
-  count: 3
-})
-
-function drawScene() {
-  regl.clear({
-    color: floatColor(settings.backgroundColor).concat([1.0]),
-    depth: 1
-  })
-  drawMote({
-    shadowColor: floatColor(settings.shadowColor),
-    lightAColor: floatColor(settings.lightAColor),
-    lightBColor: floatColor(settings.lightBColor),
-    objectPosition: physics.getObjectPosition(),
-    scale: settings.objectScale
-  })
-}
-
-function floatColor(intColor) {
-  if (typeof intColor === 'string') {
-    const matches = intColor.match(/#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/)
-    intColor = matches.slice(1, 4).map(string => parseInt(string, 16))
+    })
   }
-  return intColor.map(value => value / 255.0)
-}
 
-regl.frame(({viewportWidth, viewportHeight}) => {
-  fbo.resize(viewportWidth, viewportHeight)
-
-  physics.step()
-
-  if (!doPostProcess) {
-    drawScene()
-  } else {
-    captureRaw({}, () => drawScene())
-    drawProcessed({decay})
+  _drawScene() {
+    regl.clear({
+      color: this._floatColor(settings.backgroundColor).concat([1.0]),
+      depth: 1
+    })
+    drawMote({
+      shadowColor: this._floatColor(settings.shadowColor),
+      lightAColor: this._floatColor(settings.lightAColor),
+      lightBColor: this._floatColor(settings.lightBColor),
+      objectPosition: physics.getObjectPosition(),
+      scale: settings.objectScale
+    })
   }
-})
 
-module.exports = {
-  trigger,
-  tap
+  _floatColor(intColor) {
+    if (typeof intColor === 'string') {
+      const matches = intColor.match(/#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/)
+      intColor = matches.slice(1, 4).map(string => parseInt(string, 16))
+    }
+    return intColor.map(value => value / 255.0)
+  }
 }
+
+export default new World()
