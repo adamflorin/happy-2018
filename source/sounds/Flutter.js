1312c1a33ad1f0cebd168329cf09181c593ce3be
Adam Florin <hey@adamflorin.work>
2018-01-02 17:10:29 -0800

    Use multiple slightly detuned synths for pad

--- source/sounds/Flutter.js
+++ source/sounds/Flutter.js
@@ -2,10 +2,17 @@ import Tone from 'tone'
 
 export default class Flutter {
   constructor(index) {
-    this._oscillator = new Tone.Oscillator({
-    	type: 'sawtooth',
-    	frequency: 220 * Math.pow(1.5, index),
-    	volume: -24.0,
+    let volume = -24.0
+
+    this._oscillator1 = new Tone.Oscillator({
+    	type: 'sine',
+    	frequency: 220.0 * Math.pow(1.5, index),
+    	volume,
+    })
+    this._oscillator2 = new Tone.Oscillator({
+    	type: 'sine',
+    	frequency: 218.0 * Math.pow(1.5, index),
+    	volume,
     })
 
     this._filter = new Tone.Filter({
@@ -14,14 +21,15 @@ export default class Flutter {
       rolloff: -48.0
     })
 
-    this._oscillator.connect(this._filter)
-
-    this._oscillator.start()
+    this._oscillator1.connect(this._filter)
+    this._oscillator2.connect(this._filter)
+    this._oscillator1.start()
+    this._oscillator2.start()
   }
 
   updateDistance(distance) {
-    distance = Math.pow(distance, 1.5)
-    let value = 50.0 + distance * 8000.0
+    distance = Math.pow(distance, 4.0)
+    let value = 0.0 + distance * 8000.0
     this._filter.frequency.value = value
   }
 
