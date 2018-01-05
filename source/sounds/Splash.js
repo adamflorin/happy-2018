866b526ca64aba2ad29bba2b2a595d8b95a857fd
Adam Florin <hey@adamflorin.work>
2018-01-04 16:45:46 -0800

    Add soft flutter back in

--- source/sounds/Splash.js
+++ source/sounds/Splash.js
@@ -30,18 +30,35 @@ export default class Splash {
   }
 
   _createSoftChain() {
+    var softGain = new Tone.Gain(1.0).connect(this.output)
+
+    this.softLFO = new Tone.LFO({
+      frequency: 20.0,
+      type: 'sawtooth',
+      min: 0.8,
+      max: 0.0
+    }).start()
+    var lfoFilter = new Tone.Filter({
+      type: 'lowpass',
+      frequency: 1000
+    })
+
     this.softFilter = new Tone.Filter({
       frequency: 5000,
       type: 'highpass',
       Q: 1.0,
       gain: 0.8
-    }).connect(this.output)
+    }).connect(softGain)
+
+    this.softLFO.chain(lfoFilter, softGain.gain)
 
     this.softEnvelope = new Tone.Volume(-12.0).connect(this.softFilter)
   }
 
   _createSource() {
     const source = new Tone.Gain()
+    const harmonicity = 1.5
+    const modulationIndex = 5000
 
     var carrier = new Tone.Oscillator({
       type: 'sine',
@@ -50,11 +67,11 @@ export default class Splash {
 
     var modulator = new Tone.Oscillator({
       type: 'sine',
-      frequency: this.frequency * 1.1,
+      frequency: this.frequency * harmonicity,
       volume: -12.0
     }).start()
 
-    var modulationNode = new Tone.Multiply(5000)
+    var modulationNode = new Tone.Multiply(modulationIndex)
 
     modulator.connect(modulationNode)
     modulationNode.connect(carrier.frequency)
@@ -76,12 +93,15 @@ export default class Splash {
     const velocityMagnitude = magnitude(lastDelta)
 
     if (positionMagnitude > 0.01) {
+      let limitedMagnitude = Math.min(0.6, positionMagnitude)
+
       // const dotProduct = dot(normalize(position), normalize(lastDelta))
       // const movingAway = (dotProduct > 0.0)
-      volume += 48.0 + (2.0 * positionMagnitude) * 48.0
+
+      volume += 48.0 + (2.0 * limitedMagnitude) * 48.0
       volume = Math.min(0.0, volume)
 
-      let eased = 1.0 - Math.pow(1.0 - positionMagnitude, 2.0)
+      let eased = 1.0 - Math.pow(1.0 - limitedMagnitude, 2.0)
       frequency = 10000.0 - eased * 9000.0
     }
 
