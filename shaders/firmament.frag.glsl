de77a9c6e36393301e096c353dd716121797d86a
Adam Florin <hey@adamflorin.work>
2018-01-11 00:07:00 -0800

    Pull apart firmament colors

--- shaders/firmament.frag.glsl
+++ shaders/firmament.frag.glsl
@@ -4,11 +4,13 @@ varying vec3 vPosition;
 
 const float yThreshold = -5.0;
 const float yGradientRange = 50.0;
+const float gradient = 0.18;
+const float highColor = 0.98;
 
 void main() {
   float shade = clamp(abs(vPosition.y - yThreshold), 0.0, yGradientRange) / yGradientRange;
   shade = 1.0 - pow(1.0 - shade, 6.0);
-  shade = mix(0.8, 0.98, shade);
+  shade = mix(highColor - gradient, highColor, shade);
   if (vPosition.y < yThreshold) {
     shade *= 0.96;
   }
